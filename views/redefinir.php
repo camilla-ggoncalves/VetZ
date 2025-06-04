66ba72daf38c72659fe608351dd23d5d7bc88e0d
Gui-A1929 <guilherme.a.souza18@aluno.senai.br>
2025-06-04 14:23:03 -0300

    atualização das paginas produzidas por mim
    .

---
+++ views/redefinir.php
@@ -0,0 +1,5 @@
+<form action="/User/redefinirSenha" method="POST">
+    <input name="email" type="email" placeholder="Seu e-mail" required>
+    <input name="nova_senha" type="password" placeholder="Nova senha" required>
+    <button type="submit">Redefinir Senha</button>
+</form>     
